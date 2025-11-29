2247d400ed05a9575e9db7849e73861dc84efd64
Imane2005-crypto <238349576+Imane2005-crypto@users.noreply.github.com>
2025-11-29 23:31:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,29 +39,7 @@ const toggle = document.getElementById("titleForm");
     });
 
 /*----------Conducteurs-----------*/
- const toggle = document.getElementById("titleForm");
-    const form = document.getElementById("commentForm");
-    const success = document.getElementById("successMessage");
-
-    // Afficher / cacher le formulaire
-    toggle.addEventListener("click", () => {
-        if (form.style.display === "none" || form.style.display === "") {
-            form.style.display = "flex";
-            success.style.display = "none"; // On cache le message si on réouvre
-        } else {
-            form.style.display = "none";
-        }
-    });
-
-    // Quand on clique sur PUBLIIER
-    form.addEventListener("submit", (e) => {
-        e.preventDefault(); // empêche la page de se recharger
-        form.style.display = "none"; // on cache le formulaire
-        success.style.display = "block"; // on affiche le message
-    });
-
-
-    const wrapper = document.querySelector('.cards-wrapper');
+ const wrapper = document.querySelector('.cards-wrapper');
 const btnRight = document.getElementById('btn-right');
 const btnLeft = document.getElementById('btn-left');
 
