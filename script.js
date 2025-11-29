a2ff5a8d246560bc51b9ee24fa8d1d1a063b2133
Imane2005-crypto <238349576+Imane2005-crypto@users.noreply.github.com>
2025-11-29 22:57:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,29 @@ const toggle = document.getElementById("titleForm");
     });
 
 /*----------Conducteurs-----------*/
-const wrapper = document.querySelector('.cards-wrapper');
+ const toggle = document.getElementById("titleForm");
+    const form = document.getElementById("commentForm");
+    const success = document.getElementById("successMessage");
+
+    // Afficher / cacher le formulaire
+    toggle.addEventListener("click", () => {
+        if (form.style.display === "none" || form.style.display === "") {
+            form.style.display = "flex";
+            success.style.display = "none"; // On cache le message si on réouvre
+        } else {
+            form.style.display = "none";
+        }
+    });
+
+    // Quand on clique sur PUBLIIER
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // empêche la page de se recharger
+        form.style.display = "none"; // on cache le formulaire
+        success.style.display = "block"; // on affiche le message
+    });
+
+
+    const wrapper = document.querySelector('.cards-wrapper');
 const btnRight = document.getElementById('btn-right');
 const btnLeft = document.getElementById('btn-left');
 
@@ -50,13 +72,14 @@ btnRight.addEventListener('click', () => {
 btnLeft.addEventListener('click', () => {
     wrapper.scrollBy({ left: -220, behavior: 'smooth' });
 });
-//background header 
-window.addEventListener("scroll", function () {
-    const header = document.getElementById("header");
 
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+
+
+
+document.querySelectorAll(".contact-conducteur").forEach(button => {
+    button.addEventListener("click", () => {
+        const card = button.closest(".card");
+        card.classList.toggle("blurred");
+        card.classList.toggle("affiche-contact");
+    });
 });
